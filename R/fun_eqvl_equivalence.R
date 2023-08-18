4291ce3e52d2db1b74bd9abf7402c9872ddbb88a
Cao Bittencourt Ferreira <caobittencourt@gmail.com>
2023-08-17 21:21:55 -0300

    improved equivalence function

--- R/fun_eqvl_equivalence.R
+++ R/fun_eqvl_equivalence.R
@@ -6,6 +6,7 @@
 # - Equivalence function ------------------------------------
 fun_eqvl_equivalence <- function(
     dbl_var
+    , dbl_scale_lb = NULL
     , dbl_scale_ub = NULL
     , dbl_scaling = 1
 ){
@@ -14,6 +15,14 @@ fun_eqvl_equivalence <- function(
   stopifnot(
     "'dbl_var' must be a numeric vector or matrix." =
       is.numeric(dbl_var)
+  )
+
+  stopifnot(
+    "'dbl_scale_lb' must be either NULL or numeric." =
+      any(
+        is.numeric(dbl_scale_lb)
+        , is.null(dbl_scale_lb)
+      )
   )
 
   stopifnot(
@@ -33,12 +42,20 @@ fun_eqvl_equivalence <- function(
   )
 
   # Normalize data to percentage scale
-  # Normalize by upper bound, if any
-  if(length(dbl_scale_ub)){
+  if(all(
+    length(dbl_scale_lb),
+    length(dbl_scale_ub)
+  )){
 
-    dbl_var /
-      dbl_scale_ub[[1]] ->
-      dbl_var
+    # Normalize by scale bounds
+    dbl_var / (
+      dbl_scale_ub[[1]] -
+        dbl_scale_lb[[1]]
+    ) -
+      dbl_scale_lb[[1]] / (
+        dbl_scale_ub[[1]] -
+          dbl_scale_lb[[1]]
+      ) -> dbl_var
 
   } else if(!all(
     max(dbl_var, na.rm = T) <= 1,
@@ -54,14 +71,18 @@ fun_eqvl_equivalence <- function(
 
   }
 
+  dbl_var[all(
+    dbl_var <= 1,
+    dbl_var >= 0
+  )] -> dbl_var
+
   # Calculate equivalence
-  dbl_var ^
-    (
-      (1 / dbl_var) ^ (
-        dbl_scaling[[1]] /
-          dbl_var
-      )
-    ) -> dbl_equivalence
+  dbl_var ^ (
+    (1 / dbl_var) ^ (
+      dbl_scaling[[1]] /
+        dbl_var
+    )
+  ) -> dbl_equivalence
 
   # Truncate NA's
   dbl_equivalence[
